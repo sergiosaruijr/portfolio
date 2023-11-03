9c88cba08184a122435b42d33820a08f2446dfff
sergiosaruijr <sergiosaruijr@gmail.com>
2023-11-03 01:39:24 -0300

    Adicionando componente header

---
+++ app/components/header/index.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { NavItem } from './nav-item'
+
+const NAV_ITEMS = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'Projetos',
+    href: '/projects',
+  },
+]
+
+export const Header = () => {
+  return (
+    <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center ">
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <Image
+            width={78}
+            height={69}
+            src="/images/logo.svg"
+            alt="Logo SS Dev"
+          />
+        </Link>
+
+        <nav className="flex items-center gap-4 sm:gap-10">
+          {NAV_ITEMS.map((item) => (
+            <NavItem {...item} key={item.label} />
+          ))}
+        </nav>
+      </div>
+    </header>
+  )
+}
---
+++ app/components/header/nav-item.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { cn } from '@/app/lib/utils'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+type NavItemProps = {
+  label: string
+  href: string
+}
+
+export const NavItem = ({ label, href }: NavItemProps) => {
+  const pathname = usePathname()
+
+  const isActive = pathname === href
+
+  return (
+    <Link
+      href={href}
+      className={cn(
+        'text-gray-400 flex items-center gap-2 font-medium font-mono',
+        isActive && 'text-gray-50',
+        // ou
+        // {
+        //   'hover:text-gray-50': 'isActive',
+        // },
+      )}
+    >
+      {/* <span className="text-violet-600">#</span> */}
+      <span className="text-emerald-400">#</span>
+      {label}
+    </Link>
+  )
+}
