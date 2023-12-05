e18e17387ecec9e3f131e5a6d0c2fc726324efb8
sergiosaruijr <sergiosaruijr@gmail.com>
2023-12-04 23:58:13 -0300

    Buscando dados do Hypraph

---
+++ app/utils/fetch-hygraph-query.ts
@@ -0,0 +1,14 @@
+export const fetchHygraphQuery = async (query: string) => {
+  const response = await fetch(process.env.HYGRAPH_URL!, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`,
+    },
+    body: JSON.stringify({ query }),
+    next: {
+      revalidate: 60 * 60 * 24, // 24 hours
+    },
+  })
+}
