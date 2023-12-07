78f92738baac5beaa12751fa0c6f6a6705d9f7ff
sergiosaruijr <sergiosaruijr@gmail.com>
2023-12-06 23:56:38 -0300

    Buscando conhecimentos Hygraph

--- app/components/pages/home/known-techs/index.tsx
+++ app/components/pages/home/known-techs/index.tsx
@@ -1,21 +1,20 @@
 import { SectionTitle } from '@/app/components/section-title'
 import { KnownCard } from './know-card'
 import { TbBrandNextjs } from 'react-icons/tb'
+import { KnownTech as IKnownTech } from '@/app/types/projects'
 
-export const KnownTechs = () => {
+type KnownTechsProps = {
+  techs: IKnownTech
+}
+
+export const KnownTechs = ({ techs }: KnownTechsProps) => {
   return (
     <section className="container py-16">
       <SectionTitle subtitle="competências" title="Conhecimentos" />
 
       <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
-        {Array.from({ length: 8 }).map((_, index) => (
-          <KnownCard
-            key={index}
-            tech={{
-              icon: <TbBrandNextjs />,
-              name: 'Next.js',
-            }}
-          />
+        {techs?.map((tech: any) => (
+          <KnownCard key={tech.name} tech={tech} />
         ))}
       </div>
     </section>
